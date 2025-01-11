66dd2d9974290de43de96e8c0123def139a8eec8
OnetoTommy <xingguangc09@gmail.com>
2025-01-10 20:06:09 -0800

    course01

---
+++ Web/course01/script.js
@@ -0,0 +1,35 @@
+console.log('Hi!')
+
+const header = document.querySelector('h1')
+header.textContent = "22"
+
+let a = 1
+let b = 2
+a = b + 2
+
+const c = 2
+    // c = a + 2
+
+let str = "hello"
+let boolean = true;
+
+let sth
+console.log(sth)
+
+// window.alert("Hello")
+
+const value = window.prompt("Give me Value")
+console.log(value)
+
+debugger
+
+const number = Number.parseInt(value)
+
+const isEvent = number % 2
+
+if (isEvent === 0) {
+    window.alert("even")
+
+} else {
+    window.alert("odd")
+}
