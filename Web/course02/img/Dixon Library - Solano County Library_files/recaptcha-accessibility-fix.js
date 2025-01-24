399afb16e4714ec4befbe252983ce880b4c75587
OnetoTommy <xingguangc09@gmail.com>
2025-01-23 19:29:15 -0800

    course02

--- Web/course02/img/Dixon Library - Solano County Library_files/recaptcha-accessibility-fix.js
+++
@@ -1,6 +0,0 @@
-jQuery(window).on('load', function () {
-    let textarea = document.getElementById("g-recaptcha-response-100000");
-    textarea.setAttribute("aria-hidden", "true");
-    textarea.setAttribute("aria-label", "do not use");
-    textarea.setAttribute("aria-readonly", "true");
-});
